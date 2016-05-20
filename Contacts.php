0084b82c3e2fc64528060e63a3f47c9b09483e52
PERRIN Olivier <perrinolivier88@gmail.com>
2016-05-20 10:19:33 +0200

    V1

---
+++ Contacts.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html class=" desktop landscape" lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<style type="text/css">.gm-style .gm-style-mtc label,.gm-style .gm-style-mtc div{font-weight:400}</style>
+<style type="text/css">.gm-style-pbc{transition:opacity ease-in-out;background-color:black;text-align:center}.gm-style-pbt{font-size:22px;color:white;font-family:Roboto,Arial,sans-serif;position:relative;margin:0;top:50%;-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%);transform:translateY(-50%)}</style>
+<link href="Contacts_fichiers/css.css" rel="stylesheet" type="text/css">
+<style type="text/css">.gm-style .gm-style-cc span,.gm-style .gm-style-cc a,.gm-style .gm-style-mtc div{font-size:10px}</style><style type="text/css">@media print {  .gm-style .gmnoprint, .gmnoprint {    display:none  }}@media screen {  .gm-style .gmnoscreen, .gmnoscreen {    display:none  }}</style>
+<!-- /*<style type="text/css">.gm-style{font-family:Roboto,Arial,sans-serif;font-size:11px;font-weight:400;text-decoration:none}.gm-style img{max-width:none}</style>*/ -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="format-detection" content="telephone=no">
+  <title>Contacts</title>
+<link href="styles.css" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="Home_files/bootstrap.css" rel="stylesheet">
+
+  <!-- Links -->
+  <link rel="stylesheet" href="Contacts_fichiers/rd-mailform.css">
+  <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+  <!--JS-->
+  <script src="Contacts_fichiers/visits.json" defer="true" async=""></script>
+  <script src="Contacts_fichiers/linkid.js" async="" type="text/javascript"></script>
+  <script style="" src="Contacts_fichiers/analytics.js" async="" type="text/javascript"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+  <!--[if lt IE 9]>
+  <div style=' clear: both; text-align:center; position: relative;'>
+    <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+      <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+           alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+    </a>
+  </div>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
+    
+    
+            <style>
+    nav > ul > li:nth-child(3) > a,
+nav > ul > li:hover > a,
+nav > ul > li > a:hover {
+    color: #fff;
+}
+nav > ul > li:nth-child(3) > a:after,
+nav > ul > li:hover > a:after,
+nav > ul > li > a:hover:after {
+    background: #f64d00;
+}
+    </style>
+    
+  <script src="Contacts_fichiers/device.js"></script>
+<script src="Contacts_fichiers/common.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/map.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/util.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/marker.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/infowindow.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/onion.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/stats.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/controls.js" charset="UTF-8" type="text/javascript"></script>
+<script data-cfasync="false" defer="defer" async="" src="Contacts_fichiers/application2.js"></script>
+<style type="text/css">.olark-key,#hbl_code,#olark-data{display: none !important;}</style>
+<link href="Contacts_fichiers/cbb092d6554938a9549cb716ffcb1f94.css" rel="stylesheet" type="text/css" id="habla_style_div">
+<style type="text/css">#olk-b-2250 .cancel-button-olk-6808,#olk-b-2250 .referral-button-olk-6808{font-family:"Helvetica Neue",HelveticaNeue,Helvetica,Arial,sans-serif !important;font-size:14px !important;font-weight:bold !important;border-radius:3px !important;border:1px solid #d65129;-webkit-font-smoothing:antialiased;width:80% !important;padding:10px 15px !important;margin-bottom:10px !important;transition:.03s;}#olk-b-2250 .cancel-button-olk-6808:hover,#olk-b-2250 .referral-button-olk-6808:hover{border:1px solid #c14925}#olk-b-2250 .cancel-button-olk-6808:focus,#olk-b-2250 .referral-button-olk-6808:focus{outline:0}#olk-b-2250{width:100% !important;display:inline-block !important}#olk-b-2250 .wrapper-olk-6808{background:#fff;border-left:1px solid #ededed;border-right:1px solid #ededed;text-align:center;width:100%;position:absolute;top:34px;right:0;bottom:0;left:0;padding:10px;box-sizing:border-box;overflow-y:auto}#olk-b-2250 .description-olk-6808{font-size:14px;font-family:"Helvetica Neue",HelveticaNeue,Helvetica,Arial,sans-serif;line-height:1.4em;margin:0 0 15px}#olk-b-2250 .cancel-button-olk-6808{background:#d65129;color:#fff;}#olk-b-2250 .cancel-button-olk-6808:hover{background:#c14925}#olk-b-2250 .referral-button-olk-6808{background:#fff;color:#d65129;}#olk-b-2250 .referral-button-olk-6808:hover{color:#c14925}#olk-b-2250 .brand-olk-6808{color:#a6aeb0 !important;font-family:"Helvetica Neue",HelveticaNeue,Helvetica,Arial,sans-serif !important;padding:0 !important;font-size:11px !important;margin:0 0 5px 10px !important;display:inline-block !important;transition:.03s;}#olk-b-2250 .brand-olk-6808:hover{color:#d65129 !important}</style><style type="text/css">@media print {#habla_beta_container_do_not_rely_on_div_classes_or_names {display: none !important}}</style></head>
+<body><div id="olark" style="display: none;"><olark><iframe id="olark-loader" frameborder="0"></iframe></olark></div>
+<div class="page">
+  <!--========================================================
+                            HEADER
+  =========================================================-->
+<header class="fix">
+  <div id="header">
+			<div class="wrap group">
+				<div class="head-wrap group">
+					<img id="logo" class="log" alt="logo" src="img/logo.png">
+					<nav>
+						
+						<ul>
+						<li><a  rel="" href="index.html" class="active">Accueil</a></li>
+							<li><a  href="commander.html">Commander</a></li>
+                            <li><a  href="Contacts.php">Contact</a></li>
+						</ul>
+					</nav>
+				</div> <!-- .head-wrap -->
+				<div class="lang-selector"><a id="langswitch" href="#" data-lang="en">FR</a></div>
+			</div> <!-- .wrap < header -->
+		</div>
+
+  <div style="top: -58px; visibility: hidden; position: fixed; width: 100%; margin-top: 0px;" id="stuck_container" class="stuck_container isStuck">
+      <div class="container container-wide">
+        <nav class="navbar navbar-default navbar-static-top ">
+          <div class="navbar-header center-xs">
+            <h1 class="navbar-brand">
+              <a href="#">Japan House</a>
+            </h1>
+          </div>
+          <ul class="navbar-nav sf-menu navbar-right sf-js-enabled sf-arrows" data-type="navbar">
+            <li class="">
+              <a href="#">Home</a>
+            </li>
+            <li class="">
+              <a href="#">About us</a>
+            </li>
+            <li class="">
+              <a href="#">Specialities</a>
+            </li>
+
+            <li class="dropdown">
+              <a class="sf-with-ul" href="#">Menu</a>
+              <ul style="display: none;" class="dropdown-menu">
+                <li>
+                  <a href="#">Nigiri and Sashimi</a>
+                </li>
+
+
+                <li>
+                  <a href="#">Vegetarian Rolls</a>
+                </li>
+                <li>
+                  <a href="#">Gunkan</a>
+                </li>
+                <li>
+                  <a class="sf-with-ul" href="#">Sushi Combination</a>
+                  <ul style="display: none;" class="dropdown-menu">
+                    <li>
+                      <a href="#">Sushi Set</a>
+                    </li>
+                    <li>
+                      <a href="#">Rolls</a>
+                    </li>
+                    <li>
+                      <a href="#">Nigiri Set</a>
+                    </li>
+                    <li>
+                      <a href="#">404</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Our Speciality Rolls</a>
+                </li>
+                <li>
+                  <a href="#">Kitchen Dishes</a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="">
+              <a href="#">Services</a>
+            </li>
+
+            <li class="active">
+              <a href="#">Contact us</a>
+            </li>
+
+          </ul>
+        </nav>
+      </div>
+    </div></header>
+
+
+  <!--========================================================
+                            CONTENT
+  =========================================================-->
+
+  <main>
+    <!-- CONTACT US -->
+    <section class="well-12 bg-image-left bg-image-left--4">
+      <div class="container container-wide text-center">
+        <h2 class="head-bord-4">
+                      CONTACTER NOUS
+        </h2>
+
+        <div class="row center-sm text-left offset-2">
+          <div class="col-md-3">
+            
+          </div>
+          <div class="col-md-6">
+            <!-- Php -->
+            <?php
+              $url = "exemple@gmail.com";
+              $nom = $_POST['nom'];
+              $prenom = $_POST['prenom'];
+              $mail = $_POST['email'];
+              $message = $_POST['message'];
+              $post = $_POST['postal'];
+              $vil = $_POST['ville'];
+              $tel = $_POST['tel'];
+              mail($url.''.$prenom.''.$nom.''.$message.''.$post.''.$vil.''.$tel.''.$mail);
+            ?>
+            <!--Fin Php-->
+            <form class="rd-mailform" method="post" action="Contacts.php">
+              <!-- RD Mailform Type -->
+              <input name="form-type" value="contact" type="hidden">
+              <!-- END RD Mailform Type -->
+
+              <div class="form-group mfInput">
+                <input id="fname" name="nom" type="text" placeholder="Prenom" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="prenom" type="text" placeholder="Nom" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="tel" type="text" placeholder="Telephone" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="ville" type="text" placeholder="Ville" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="postal" type="text" placeholder="Code Postal" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="inputEmail" name="email" type="email" placeholder="Adresse mail" class="form-control"required></div>
+
+
+              <div class="form-group textarea mfInput">
+                <textarea class="form-control" id="message" name="message" placeholder="Message" rows="7"required></textarea></div>
+
+              <div class="form-group btn-wr">
+                <button class="btn btn-lg btn-primary" type="submit">Envoyer</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END CONTACT US -->
+  </main>
+
+  <!--========================================================
+                          FOOTER
+  =========================================================-->
+ <footer>
+    <section>
+      <div class="container container-wide center-sm">
+        <div class="row">
+          <div class="col-md-5 ">
+            <p class="rights">
+              © <span id="copyright-year">2016</span> Clerio, TOUS LES DROITS RESERVES.
+            </p>
+          </div>
+          <div class="col-md-2 text-center ">
+            <a href="#" class="brand-name"> Clerio</a>
+          </div>
+          <div class="col-md-offset-2 col-md-3 ">
+            <ul class="inline-list">
+              <li><a href="#" class="icon icon-md icon-default fa-facebook"></a></li>
+              <li><a href="#" class="icon icon-md icon-default  fa-pinterest-p"></a></li>
+              <li><a href="#" class="icon icon-md icon-default fa-twitter"></a></li>
+              <li><a href="#" class="icon icon-md icon-default fa-google-plus"></a></li>
+              <li><a href="#" class="icon icon-md icon-default fa-instagram"></a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  </footer>
+</div>
+
